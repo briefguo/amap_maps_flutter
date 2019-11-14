fab48d41b6a19c33061f36ea375f3cf36448b413
briefguo <gyj9094@gmail.com>
2019-11-14 11:53:47 +0800

    feat: example更新ios配置

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/briefguo/Public/bin/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/briefguo/Public/app/amap_maps_flutter/example"
+export "FLUTTER_TARGET=/Users/briefguo/Public/app/amap_maps_flutter/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/briefguo/Public/bin/flutter/bin/cache/artifacts/engine/ios"
